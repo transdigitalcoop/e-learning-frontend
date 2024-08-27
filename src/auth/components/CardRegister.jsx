4d8e739336962df57fc32ae32e64607fcf689c9b
FILIPRAIDER <filipraider123@gmail.com>
2024-08-27 16:07:33 -0500

    Version actualizada del register

--- src/auth/components/CardRegister.jsx
+++ src/auth/components/CardRegister.jsx
@@ -3,10 +3,10 @@ import axios from "axios";
 import { useState } from "react";
 export const CardRegister = () => {
   const [formData, setFormData] = useState({
-    nombre1: "",
-    nombre2: "",
-    apellido1: "",
-    apellido2: "",
+    name: "",
+    segundo_nombre: "",
+    primer_apellido: "",
+    segundo_apellido: "",
     email: "",
     contraseña: "",
   });
@@ -42,26 +42,46 @@ export const CardRegister = () => {
           <div className="name">
             <div className="inputs">
               <label>Primer Nombre:</label>
-              <input type="text" onChange={handleChange} required />
+              <input type="text" onChange={handleChange} name="name" required />
             </div>
             <div className="inputs">
               <label>Segundo Nombre:</label>
-              <input type="text" onChange={handleChange} required />
+              <input
+                type="text"
+                onChange={handleChange}
+                name="segundo_nombre"
+                required
+              />
             </div>
             <div className="inputs">
               <label>Primer Apellido:</label>
-              <input type="text" onChange={handleChange} required />
+              <input
+                type="text"
+                onChange={handleChange}
+                name="primer_apellido"
+                required
+              />
             </div>
             <div className="inputs">
               <label>Segundo Apellido:</label>
-              <input type="text" onChange={handleChange} required />
+              <input
+                type="text"
+                onChange={handleChange}
+                name="segundo_apellido"
+                required
+              />
             </div>
           </div>
           <div className="account">
             <label>Correo:</label>
-            <input type="email" onChange={handleChange} required />
+            <input type="email" onChange={handleChange} name="email" required />
             <label>Contraseña:</label>
-            <input type="password" onChange={handleChange} required />
+            <input
+              type="password"
+              onChange={handleChange}
+              name="contraseña"
+              required
+            />
             <label>Confirmar contraseña:</label>
             <input type="text" onChange={handleChange} required />
           </div>
